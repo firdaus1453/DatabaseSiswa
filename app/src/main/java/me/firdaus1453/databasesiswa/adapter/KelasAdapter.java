f03126ea353138fc8072b0d3e46132a62c60f004
firdaus1453 <mfirdaus1453@gmail.com>
2019-01-18 15:28:46 +0800

    Operation update and back button on actionbar

--- app/src/main/java/me/firdaus1453/databasesiswa/adapter/KelasAdapter.java
+++ app/src/main/java/me/firdaus1453/databasesiswa/adapter/KelasAdapter.java
@@ -1,14 +1,21 @@
 package me.firdaus1453.databasesiswa.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 import java.util.Random;
@@ -16,7 +23,10 @@ import java.util.Random;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.firdaus1453.databasesiswa.R;
+import me.firdaus1453.databasesiswa.db.Constant;
+import me.firdaus1453.databasesiswa.db.SiswaDatabase;
 import me.firdaus1453.databasesiswa.model.KelasModel;
+import me.firdaus1453.databasesiswa.ui.UpdateKelasActivity;
 
 /**
  * Created by firdaus1453 on 1/17/2019.
@@ -28,8 +38,14 @@ public class KelasAdapter extends RecyclerView.Adapter<KelasAdapter.ViewHolder> 
     // Membuat variable list dengan cetakan KelasModel
     private final List<KelasModel> kelasModelList;
 
+    // Variable database
+    private SiswaDatabase siswaDatabase;
+
     // Membuat variable bundle untuk membawa data
     private Bundle bundle;
+
+    // Membuat variable Alertdialog
+    private android.support.v7.app.AlertDialog alertDialog;
 
     public KelasAdapter(Context context, List<KelasModel> kelasModelList) {
         this.context = context;
@@ -42,7 +58,7 @@ public class KelasAdapter extends RecyclerView.Adapter<KelasAdapter.ViewHolder> 
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, final int position) {
         // Memindahkan data di dalam list dengan index position ke dalam KelasModel
         final KelasModel kelasModel = kelasModelList.get(position);
         // Menampilkan data ke layar
@@ -57,6 +73,65 @@ public class KelasAdapter extends RecyclerView.Adapter<KelasAdapter.ViewHolder> 
 
         // mesetting color background cardview
         holder.cvKelas.setCardBackgroundColor(color);
+
+        // Membuat onclick icon overflow
+        holder.overflow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+
+                // Buat object database
+                siswaDatabase = siswaDatabase.createDatabase(context);
+
+                // Membuat object popumemu
+                PopupMenu popupMenu = new PopupMenu(context, view);
+
+                // Inflate menu ke dalam popupmenu
+                popupMenu.inflate(R.menu.popup_menu);
+
+                // Menampilkan menu
+                popupMenu.show();
+
+                // Onclick pada salah satu menu pada popupmenu
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()){
+                            case R.id.delete:
+                                // Melakukan operasi delete data
+                                siswaDatabase.kelasDao().delete(kelasModel);
+
+                                // Menghapus data yang telash di hapus pada List
+                                kelasModelList.remove(position);
+
+                                // Memberitahu bahwa data sudah hilang
+                                notifyItemRemoved(position);
+                                notifyItemRangeChanged(0, kelasModelList.size());
+
+                                Toast.makeText(context, "Berhasil dihapus", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.edit:
+                                // Membuat object bundle
+                                bundle = new Bundle();
+
+                                // Mengisi bundle dengan data
+                                bundle.putInt(Constant.KEY_ID_KELAS, kelasModel.getId_kelas());
+                                bundle.putString(Constant.KEY_NAMA_KELAS, kelasModel.getNama_kelas());
+                                bundle.putString(Constant.KEY_NAMA_WALI, kelasModel.getNama_wali());
+
+                                // Berpindah halaman dengan membawa data
+                                context.startActivity(new Intent(context, UpdateKelasActivity.class).putExtras(bundle));
+                                break;
+                        }
+                        return true;
+                    }
+                });
+
+
+
+
+            }
+        });
+
     }
 
     @Override
@@ -71,6 +146,8 @@ public class KelasAdapter extends RecyclerView.Adapter<KelasAdapter.ViewHolder> 
         TextView tvNamaWali;
         @BindView(R.id.cvKelas)
         CardView cvKelas;
+        @BindView(R.id.overflow)
+        ImageButton overflow;
 
         public ViewHolder(View itemView) {
             super(itemView);
--- app/src/main/java/me/firdaus1453/databasesiswa/ui/UpdateKelasActivity.java
+++ app/src/main/java/me/firdaus1453/databasesiswa/ui/UpdateKelasActivity.java
@@ -1,15 +1,102 @@
 package me.firdaus1453.databasesiswa.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import me.firdaus1453.databasesiswa.R;
+import me.firdaus1453.databasesiswa.db.Constant;
+import me.firdaus1453.databasesiswa.db.SiswaDatabase;
+import me.firdaus1453.databasesiswa.model.KelasModel;
 
 public class UpdateKelasActivity extends AppCompatActivity {
+
+    @BindView(R.id.edtNamaKelas)
+    EditText edtNamaKelas;
+    @BindView(R.id.edtNamaWali)
+    EditText edtNamaWali;
+    @BindView(R.id.btnSimpan)
+    Button btnSimpan;
+
+    // Membuat variable bundle
+    private Bundle bundle;
+
+    // Membuat variable list
+    private List<KelasModel> kelasModelList;
+
+    // Membuat variable database
+    private SiswaDatabase siswaDatabase;
+
+    private int id_kelas;
+    private String nama_kelas, nama_wali;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_kelas);
+        ButterKnife.bind(this);
+
+        // Set title
+        setTitle("Update data kelas");
+
+        // Menangkap data dari activity sebelumnya
+        bundle = getIntent().getExtras();
+
+        // Buat object list
+        kelasModelList = new ArrayList<>();
+
+        // Buat object database
+        siswaDatabase = SiswaDatabase.createDatabase(this);
+
+        // Menampilkan data sebelumnya ke layar
+        showData();
+    }
+
+    private void showData() {
+        // mengambil data di dalam bundle
+        id_kelas = bundle.getInt(Constant.KEY_ID_KELAS);
+        nama_kelas = bundle.getString(Constant.KEY_NAMA_KELAS);
+        nama_wali = bundle.getString(Constant.KEY_NAMA_WALI);
+
+        // Menampilkan ke layar
+        edtNamaKelas.setText(nama_kelas);
+        edtNamaWali.setText(nama_wali);
+    }
+
+    @OnClick(R.id.btnSimpan)
+    public void onViewClicked() {
+        // Mengambil data
+        getData();
+
+        // Mengirim data ke sqlite
+        saveData();
+
+        Toast.makeText(this, "Berhasil di update", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    private void getData() {
+        // Mengambil inputan user dan dimasukkan ke dalam variable
+        nama_kelas = edtNamaKelas.getText().toString();
+        nama_wali = edtNamaWali.getText().toString();
+    }
+
+    private void saveData() {
+        // Membuat object kelasmodel
+        KelasModel kelasModel = new KelasModel();
+        // Memasukkan data ke kelasmodel
+        kelasModel.setId_kelas(id_kelas);
+        kelasModel.setNama_kelas(nama_kelas);
+        kelasModel.setNama_wali(nama_wali);
+        // Melakukan operasi update untuk mengupdate data ke sqlite
+        siswaDatabase.kelasDao().update(kelasModel);
     }
 }
